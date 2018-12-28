61d1c82b9feafa7d4e0d984363e41e750143a1ab
Neadlahmed <45720233+Neadlahmed@users.noreply.github.com>
2018-12-28 22:33:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,6 +90,21 @@ if (message.content === '!ninja_elarab') {
 });
 
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var prefix = "!";
+
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+ 
+client.login("");
 
 
 
