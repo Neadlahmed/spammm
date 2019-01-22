59c9c733b322883ad665cb303ad198eceb168ee3
Neadlahmed <45720233+Neadlahmed@users.noreply.github.com>
2019-01-22 15:49:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-const myid = [''];
-const prefix = ['-'];
+const myid = ['537243479923752980'];
+const prefix = ['!'];
 
 client.on('ready', () => {
    console.log(`----------------`);
